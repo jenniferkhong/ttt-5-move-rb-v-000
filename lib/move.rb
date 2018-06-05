6e2bb5c1235c3d7e8755b2a07a35666065790c51
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-05 20:29:05 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,9 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, location, current player = "X")
-  board[location.to_i-1] = current player
+def input_to_index(input)
+  input.to_i-1
 end
 
-def input_to_index("1"-"9")
+def move(board, location, current player = "X")
+  puts board[location.to_i-1] = current player
 end
